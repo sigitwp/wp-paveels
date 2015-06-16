7c403ac9a1b1b6651f525c089361f3075f0b277f
gustiazis <gooazs@gmail.com>
2015-06-16 10:51:26 +0700

    Left Behind From Last Commit

--- scripts/wp.js
+++ scripts/wp.js
@@ -1,10 +1,1 @@
 /* WP Script */
-
-/* Owl Carousel - Full Slide */
-$(document).ready(function() {
-	$('.full-slide').owlCarousel({
-		slideSpeed : 300,
-		paginationSpeed : 400,
-		singleItem:true
-	});
-});
